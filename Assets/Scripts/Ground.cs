fc9731c925c9160ae31bba66a61e490a21c449dd
Raymanni <ejp.heikkila@gmail.com>
2018-01-27 13:19:45 +0200

    Add more specific controlling

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+
+
+	void Start () {
+        //Set the depth to y so the terraina appears behind toher terrain pieces
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
     private float moveSize = 0.64f;
 
+    //States
+    bool dead = false;
+    public int dir = 0;
+    //0 = Right;
+    //1 = Down;
+    //2 = Left;
+    //3 = Up;
+
+    //Components
+    public Animator anim;
+
 	void Start () {
-		
+        anim = GetComponent<Animator>();
 	}
 	
 	void Update () {
@@ -16,42 +28,88 @@ public class Player : MonoBehaviour {
         Vector2 orig = new Vector2(transform.position.x, transform.position.y);
 
         //Draw debug raycast lines
-        Debug.DrawRay(orig, Vector2.up * 0.64f, Color.red);
-        Debug.DrawRay(orig, Vector2.down * 0.64f, Color.red);
-        Debug.DrawRay(orig, Vector2.left * 0.64f, Color.red);
-        Debug.DrawRay(orig, Vector2.right * 0.64f, Color.red);
-        //Movement with WASD
-        if (Input.GetKeyDown(KeyCode.W))
+        //Debug.DrawRay(orig, Vector2.down * 0.16f, Color.red);
+        //Raycast
+        RaycastHit2D ray = Physics2D.Raycast(orig, Vector2.down, 0.16f);
+
+        //If ground under feet, move with WASD
+        if (ray && !dead)
         {
-            RaycastHit2D ray = Physics2D.Raycast(orig, Vector2.up, 0.64f);
-            if (!ray)
+            if (Input.GetKeyDown(KeyCode.R))
             {
-                transform.position += new Vector3(0, moveSize);
+                Turn90Degrees();
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                MoveForward();
             }
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        else
         {
-            RaycastHit2D ray = Physics2D.Raycast(orig, Vector2.down, 0.64f);
-            if (!ray)
+            //Die if no ground under feet
+            if (!dead)
             {
-                transform.position += new Vector3(0, -moveSize);
+                dead = true;
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+                anim.Play("AnimPlayerDie");
             }
         }
-        if (Input.GetKeyDown(KeyCode.D))
+
+        //If dead, just fall down
+        if (dead)
         {
-            RaycastHit2D ray = Physics2D.Raycast(orig, Vector2.right, 0.64f);
-            if (!ray)
+            transform.position = new Vector3(transform.position.x, transform.position.y-0.1f, transform.position.z);
+            if (transform.localScale.x > 0)
             {
-                transform.position += new Vector3(moveSize, 0);
+                transform.localScale = new Vector3(transform.localScale.x - 0.05f, transform.localScale.y - 0.05f, transform.localScale.z - 0.05f);
             }
         }
-        if (Input.GetKeyDown(KeyCode.A))
+
+        //Animations
+        if (!dead)
         {
-            RaycastHit2D ray = Physics2D.Raycast(orig, Vector2.left, 0.64f);
-            if (!ray)
-            {
-                transform.position += new Vector3(-moveSize, 0);
-            }
+            if (dir == 0) { anim.Play("AnimPlayerRight"); }
+            if (dir == 1) { anim.Play("AnimPlayerDown"); }
+            if (dir == 2) { anim.Play("AnimPlayerLeft"); }
+            if (dir == 3) { anim.Play("AnimPlayerUp"); }
+        }
+
+        //Restart scene
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+    public void MoveForward()
+    {
+        if (dir==3)
+        {
+            transform.position += new Vector3(0, moveSize);
+        }
+        if (dir==1)
+        {
+            transform.position += new Vector3(0, -moveSize);
+        }
+        if (dir==0)
+        {
+            transform.position += new Vector3(moveSize, 0);
+        }
+        if (dir==2)
+        {
+            transform.position += new Vector3(-moveSize, 0);
+        }
+    }
+
+    public void Turn90Degrees()
+    {
+        if (dir==3)
+        {
+            dir = 0;
+        }
+        else
+        {
+            dir += 1;
         }
     }
 }
